e5c33da6d5a358d15b3d0fd23c7be3c84df54277
kojira <kojiran@gmail.com>
2026-03-21 21:13:57 +0900

    chore: remove unused dashboard Leptos/Dioxus crate

--- dashboard/src/components/mod.rs
+++
@@ -1,9 +0,0 @@
-mod sidebar;
-mod header;
-mod agent_card;
-mod skill_editor;
-
-pub use sidebar::Sidebar;
-pub use header::Header;
-pub use agent_card::AgentCard;
-pub use skill_editor::SkillEditor;
--- dashboard/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod app;
-mod api;
-mod routes;
-mod components;
-
-fn main() {
-    dioxus::launch(app::App);
-}
